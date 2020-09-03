31457916de4ab668cf9b1a3dbce1a28f58988af7
OlegZhdanov <stalker08@gmail.com>
2020-09-03 12:17:52 +0500

    homework_7 task_1 complete

--- lesson_7/task_1.py
+++ lesson_7/task_1.py
@@ -2,8 +2,9 @@
 # промежутке [-100; 100). Выведите на экран исходный и отсортированный массивы.
 
 import random
+import cProfile
 
-SIZE = 100
+SIZE = 10000
 MIN_ITEM = -100
 MAX_ITEM = 100
 array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
@@ -11,16 +12,36 @@ print(array)
 
 # array = [9, 5, 3, 8, 0, 1, 2, 6, 7, 4]
 
-n = 1
-while n < len(array):
-    is_changed = False
-    for i in range(len(array) - n):
-        if array[i] < array[i + 1]:
-            array[i], array[i + 1] = array[i + 1], array[i]
-            is_changed = True
-    if not is_changed:
-        break
-    n += 1
-    # print(array)
 
+def bubble_classic(arr):
+    n = 1
+    while n < len(arr):
+        for i in range(len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        n += 1
+
+
+def bubble_fast(arr):
+    n = 1
+    while n < len(arr):
+        is_changed = False
+        for i in range(len(arr) - n):
+            if arr[i] < arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                is_changed = True
+        if not is_changed:
+            break
+        n += 1
+        # print(array)
+
+
+cProfile.run('bubble_classic(array)')
+# 1   14.701   14.701   14.704   14.704 task_1.py:16(bubble_classic)
+random.shuffle(array)
+cProfile.run('bubble_fast(array)')
+# 1    9.548    9.548    9.550    9.550 task_1.py:25(bubble_fast)
 print(array)
+
+# Из теста видно, что оптимизация дала свои результаты. Во внутреннем цикле перебираем массив не до конца, а только
+# несортированную часть массива, а также используем флаг для проверки массива на отсортированность.
