462b6f609913a035dd2c4a43ec70b2591141e524
OlegZhdanov <stalker08@gmail.com>
2020-09-03 12:02:28 +0500

    homework_7 task_1

--- lesson_7/task_1.py
+++ lesson_7/task_1.py
@@ -1,14 +1,26 @@
 # 1). Отсортируйте по убыванию методом пузырька одномерный целочисленный массив, заданный случайными числами на
 # промежутке [-100; 100). Выведите на экран исходный и отсортированный массивы.
 
-array = [9, 5, 3, 8, 0, 1, 2, 6, 7, 4]
+import random
+
+SIZE = 100
+MIN_ITEM = -100
+MAX_ITEM = 100
+array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+print(array)
+
+# array = [9, 5, 3, 8, 0, 1, 2, 6, 7, 4]
 
 n = 1
 while n < len(array):
-    for i in range(len(array) - 1):
-        if array[i] > array[i + 1]:
+    is_changed = False
+    for i in range(len(array) - n):
+        if array[i] < array[i + 1]:
             array[i], array[i + 1] = array[i + 1], array[i]
+            is_changed = True
+    if not is_changed:
+        break
     n += 1
-    print(array)
+    # print(array)
 
 print(array)
